3888242051a71e0ee03ed707bb54b66537c8de05
Adib Oubadriss <61279217+Adiboubadriss2020@users.noreply.github.com>
2022-08-15 14:17:16 +0000

    Update ChargeRepository.java

--- src/main/java/com/fellah/api/repository/ChargeRepository.java
+++ src/main/java/com/fellah/api/repository/ChargeRepository.java
@@ -28,7 +28,7 @@ public interface ChargeRepository extends JpaRepository<Charge, Long>{
 			value = "SELECT ch.date_charge,(f.transaction,e.salaire,al.prix_arrivage,a.prix_achat,vi.prix_visite,v.transaction,c.transaction"
 					+ " FROM charge ch,fournisseur f,employee e,client c,alimentation al,animal a,veterinaire v, visite vi where"
 					+ "ch.fournisseur_id=f.id or ch.employee_id=e.id or ch.client_id = c.id or ch.animal_id=a.id or ch.alimentation_id=al.id"
-					+ "ch.visite_id=vi.id or ch.veterinaire_id=v.id and ch.date_charge between ?1 and ?2  group by ch.date_charge order by ch.date_charge  ", 
+					+ " or sch.visite_id=vi.id or ch.veterinaire_id=v.id and ch.date_charge between ?1 and ?2  group by ch.date_charge order by ch.date_charge  ", 
 			nativeQuery = true)
 			List<Object> getfilter(LocalDate threeDaysAgoDate,LocalDate today);
 }
